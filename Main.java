14ddd79f653c9b701bcb42871b9c35b73218a8a2
Mahammad Abubakker Siddik <mahammad.a.siddik@gmail.com>
2025-11-22 19:51:28 +0530

    Refactor main method for Book operations
    Updated main method to use displayDetails and borrowBook methods for Book objects.

--- Main.java
+++ Main.java
@@ -1,20 +1,21 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        // Creating Book objects
+        // Since both files are in the same folder, Main can see Book automatically!
+        Book b1 = new Book("Java Mastery", "Sundar Pichai", 250);
+        Book b2 = new Book("Programming Tactics", "Mark Munroe", 200);
 
-        //objects of class book
-        Book b1 = new Book("Java mastery", "sundar pichai", 250, false);
-        Book b2 = new Book("Programming tactics", "mark munroe", 200, false);
+        // Simulating Library Operations
+        b1.displayDetails();
+        b2.displayDetails();
+
+        System.out.println("\n--- Attempting to borrow 'Java Mastery' ---");
+        b1.borrowBook();  // Action method
         
-        //printing book details
-        System.out.println("BOOK1:  title: "+b1.title+"/ author: "+b1.author+"/ pages: "+b1.pages+"/ checkedout: "+b1.isCheckedOut);
-        System.out.println("BOOK2:  title: "+b2.title+"/ author: "+b2.author+"/ pages: "+b2.pages+"/ checkedout: "+b2.isCheckedOut);
-
-        //updating isCheckOut
-        b1.isCheckedOut = true;
-
-        //final print statement
-        System.out.println("BOOK1 updated checkout: "+b1.isCheckedOut);
+        System.out.println("\n--- Updated Details ---");
+        b1.displayDetails(); // Shows it is now checked out
         
-
+        System.out.println("\n--- Attempting to borrow 'Java Mastery' again ---");
+        b1.borrowBook(); // Should show error that it is already borrowed
     }
 }
